25249ddb1f2e130a00ea81b387f53c2ec6d93dfb
plmknbuhv <plmknbuhv@gmail.com>
2025-06-30 13:55:27 +0900

    .

--- SceneManager.cpp
+++ SceneManager.cpp
@@ -4,22 +4,22 @@
 GameScene* SceneManager::sceneList[] =
 {
 	new GameScene(1, 2), // 튜토리얼 스테이지
-	//new GameScene(2, 2),
-	//new GameScene(3, 2),
-	//new GameScene(4, 2),
-	//new GameScene(5, 2),
-	//new GameScene(6, 2), // 사과 스테이지
-	//new GameScene(7, 2),
-	//new GameScene(8, 2),
-	//new GameScene(9, 2),
-	//new GameScene(10, 2),
-	//new GameScene(11, 2), // 박스 스테이지
-	//new GameScene(12, 2),
-	//new GameScene(13, 2),
-	//new GameScene(14, 2),
-	//new GameScene(15, 2),
-	//new GameScene(16, 2),
-	//new GameScene(17, 2), // 마지막 스테이지
+	new GameScene(2, 2),
+	new GameScene(3, 2),
+	new GameScene(4, 2),
+	new GameScene(5, 2),
+	new GameScene(6, 2), // 사과 스테이지
+	new GameScene(7, 2),
+	new GameScene(8, 2),
+	new GameScene(9, 2),
+	new GameScene(10, 2),
+	new GameScene(11, 2), // 박스 스테이지
+	new GameScene(12, 2),
+	new GameScene(13, 2),
+	new GameScene(14, 2),
+	new GameScene(15, 2),
+	new GameScene(16, 2),
+	new GameScene(17, 2), // 마지막 스테이지
 };
 
 SceneManager::SceneManager() : Single()
